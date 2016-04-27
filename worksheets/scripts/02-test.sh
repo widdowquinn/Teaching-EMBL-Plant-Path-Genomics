54aa274d460440bb4f786722f3dacfcbc2b88ce7
Leighton Pritchard <leighton.pritchard@hutton.ac.uk>
2016-04-27 18:34:04 +0100

    Got worksheet 02 test script working on OSX

--- worksheets/scripts/02-test.sh
+++ worksheets/scripts/02-test.sh
@@ -11,31 +11,59 @@
 echo "Creating output directory"
 mkdir -p 02-testout
 
+# Locations of all worksheet data
+wsdatadir="../02-data"
+
+# Directory for output
+outdir="02-testout"
+
+# Stems for the three isolates of each genus. Labelled a, b, c to
+# correspond to the order we want them in the final figure (to 
+# match the source paper)
+# Serratia
+s_a="S_symbiotica_Tucson/GCF_000186485.1_ASM18648v1_genomic"
+s_b="S_proteamaculans_568/GCF_000018085.1_ASM1808v1_genomic"
+s_c="S_marcesens_FGI94/GCF_000330865.1_ASM33086v1_genomic"
+# Vibrio
+v_a="V_cholerae_El_Tor/GCF_000006745.1_ASM674v1_genomic"
+v_b="V_mimicus_ATCC_33654/GCF_001558475.1_ASM155847v1_genomic"
+v_c="V_cholerae_O395/GCF_000016245.1_ASM1624v1_genomic"
+
 # Carry out BLASTN comparisons between genomes, generating
 # .tab file output suitable for ACT
-# Pba vs Pst
-echo "Running Pba vs YpK"
-blastn -query ../02-data/P_atrosepticum_SCRI1043/GCF_000011605.1_ASM1160v1_genomic.fna \
-       -subject ../02-data/Y_pestis_KIM/GCF_000006645.1_ASM664v1_genomic.fna \
-       -outfmt 6 -out 02-testout/Pba_vs_YpK.tab
-# Pba vs Dze
-echo "Running Pba vs Dze"
-blastn -query ../02-data/P_atrosepticum_SCRI1043/GCF_000011605.1_ASM1160v1_genomic.fna \
-       -subject ../02-data/D_zeae_Ech586/GCF_000025065.1_ASM2506v1_genomic.fna \
-       -outfmt 6 -out 02-testout/Pba_vs_Dze.tab
-# Pba vs Sty
-echo "Running Pba vs Sty"
-blastn -query ../02-data/P_atrosepticum_SCRI1043/GCF_000011605.1_ASM1160v1_genomic.fna \
-       -subject ../02-data/S_enterica_CT18/GCF_000195995.1_ASM19599v1_genomic.fna \
-       -outfmt 6 -out 02-testout/Pba_vs_Sty.tab
+# Serratia
+echo "Running Serratia BLAST"
+echo "A v B"
+blastn -query ${wsdatadir}/${s_a}.fna \
+       -subject ${wsdatadir}/${s_b}.fna \
+       -outfmt 6 -out ${outdir}/serratia_A_vs_B.tab
+echo "B v C"
+blastn -query ${wsdatadir}/${s_b}.fna \
+       -subject ${wsdatadir}/${s_c}.fna \
+       -outfmt 6 -out ${outdir}/serratia_B_vs_C.tab
+# Vibrio
+echo "Running Vibrio BLAST"
+echo "A v B"
+blastn -query ${wsdatadir}/${v_a}.fna \
+       -subject ${wsdatadir}/${v_b}.fna \
+       -outfmt 6 -out ${outdir}/serratia_A_vs_B.tab
+echo "B v C"
+blastn -query ${wsdatadir}/${v_b}.fna \
+       -subject ${wsdatadir}/${v_c}.fna \
+       -outfmt 6 -out ${outdir}/serratia_B_vs_C.tab
+
 
 # The command below will open all comparisons correctly if
 # ACT is installed to run from the command-line (on OSX a
 # different command-line may be required, or an alias to
 # /Applications/ACT.app/Contents/MacOS/act)
-echo "Opening ACT"
-act $(pwd)/../02-data/D_zeae_Ech586/GCF_000025065.1_ASM2506v1_genomic.fna \
-    $(pwd)/02-testout/Pba_vs_Dze.tab \
-    $(pwd)/../02-data/P_atrosepticum_SCRI1043/GCF_000011605.1_ASM1160v1_genomic.fna \
-    $(pwd)/02-testout/Pba_vs_Sty.tab \
-    $(pwd)/../02-data/S_enterica_CT18/GCF_000195995.1_ASM19599v1_genomic.fna
+echo "Opening ACT for Serratia"
+act=/Applications/ACT.app/Contents/MacOS/act
+
+cmd="${act} $(pwd)/${wsdatadir}/${s_a}.fna \
+     $(pwd)/${outdir}/serratia_A_vs_B.tab \
+     $(pwd)/${wsdatadir}/${s_b}.fna \
+     $(pwd)/${outdir}/serratia_B_vs_C.tab \
+     $(pwd)/${wsdatadir}/${s_c}.fna"
+echo ${cmd}
+eval ${cmd}
